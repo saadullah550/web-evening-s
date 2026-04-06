c5bda5164ffd8a96b33f21dcc8dfe60e60d5c2d5
saadullah550 <saadullahjutt1205@gmail.com>
2026-04-06 18:03:08 +0500

    new

---
+++ html evening/script/index.js
@@ -0,0 +1,28 @@
+ // runs inside the browesr 
+
+ // variables and data types 
+ let name ='saad' ;  //string
+ let age =20  ;     //integer
+ let city ='Lahore' ;
+ let marks =90.7 ;  //float
+ const cnic = '123-456-789'; //string
+ //every thing written in string can show as it is
+ let status =false;  //boolien
+
+ //output
+ console.log(name)
+ console.log(age)
+ console.log(city)
+ console.log(status)
+
+ // operators: arthmatic assignment comprasion
+ // function
+ //defining a function 
+  function greet(name) //paramaters
+  {
+    //function block
+    // console.log('Hello,Guest')
+    console.log('Hello,' +name)
+  }
+  //function calling
+  greet(Abdullah)
