5658ac55db3ca0bf333976f295a558cec9c7df91
saadullah550 <saadullahjutt1205@gmail.com>
2026-04-10 19:04:33 +0500

    new

---
+++ html evening/script/index-practic.js
@@ -0,0 +1,62 @@
+ let name ='saad';
+ let age=20;
+ let city= 'Lahore'
+ let marks=99.9;
+ const cnic='123-456-789'
+ let status ='fasle'
+
+console.log(name)
+console.log(age)
+console.log(city)
+console.log(status)
+
+// function greet(name)
+
+// console.log('Hello',+ name)
+// greet('saad')
+// document.getElementById('text').innerText='Hello saad';
+// document.querySelector('h1').innerText='Hello saad'
+
+// let txt=document.getElementById('text')
+// let btn=document.getElementById('btn')
+//   let cities =['Lahore ', 'Karachi ', 'multan']
+//   console.log(cities)
+
+//   })
+
+
+
+// let btn =document.getElementById('btn')
+// let txt=document.getElementById('text')
+// btn.addEventListener('click', ()=>{
+//     let color=document.getElementById('color').value 
+//     txt.style.color=color
+// })
+
+
+
+
+
+
+
+function addvalues(a,b){
+    console.log(a+b)
+}
+addvalues(10 ,15)
+
+
+let  sum = (a,b) =>{
+    console.log(a+b)
+}
+sum(20,15)
+
+
+let cities=[ 'Lahore', 'Multan', 'Karachi']
+console.log(cities)
+
+console.log[0]
+
+
+
+
+ 
--- html evening/script/index.js
+++ html evening/script/index.js
@@ -45,22 +45,85 @@
   //using event listners with a named function 
 
   // document.getElementById('btn').addEventListener('click',changecolor)
+
   // Eventlinstner with anonymous function
+
   // document.getElementById('btn').addEventListener(
     // 'click ', function(){
       // document.querySelector('h1').style.color='red'
     // }
   // )
-  let box=document.getElementById('box')
-  document.getElementById('btn').addEventListener('click',function()
+  // let box=document.getElementById('box')
+  // document.getElementById('btn').addEventListener('click',function()
+// 
+// {
+  // box.style.backgroundColor='blue';
+  // box.style.color='white';
+  // box.style.border='2px solid gray';
+  // box.style.padding='10px';
+// })
 
-{
-  box.style.backgroundColor='blue';
-  box.style.color='white';
-  box.style.border='2px solid gray';
-  box.style.padding='10px';
-})
+// let btn=document.getElementById('btn')
+// let input=document.getElementById('input')
+// let txt = document.getElementById('text')
+
+// btn.addEventListener('click', function(){
+
+  // let val = input.value 
+  // if(val.length >= 5 ){
+    //  txt.innerText= val
+
+  // }
+  // else{
+    // txt.innerText ='Number of chars should be equal to 5 or greater than 5'
+  // }
+ 
+
+// })
+
+//  change color with user input
+
+// let btn =document.getElementById('btn')
+//   let txt =document.getElementById('text')
+// btn.addEventListener('click' , function(){
+//     let color =document.getElementById('color').value 
+//     txt.style.color =color
+//   } )
+
+//regular function 
+function addvalues(a,b){
+  console.log(a+b)
+}
+addvalues(5,7)
+// Arrow function
+let sum =(a,b) =>{
+  console.log(a+b)
+}
+sum(2,5)
 
 
+let btn =document.getElementById('btn')
+  let txt =document.getElementById('text')
+btn.addEventListener('click' , ()=>{
+    let color =document.getElementById('color').value 
+    txt.style.color =color
+  } )
+  //Arrays : is a collection of values stored in a single variable
+  // let city='lahore'
+  let cities =[ 'Lahore', 'Karachi', 'Qutta']
+  console.log(cities)
 
+  // Accessing a single value with indexing 
+  console.log[0]
+  let x =cities[1]
+  console.log(x)
 
+  cities[2]='Multan' //assigning a new value to index
+  cities[3]='Quetta' //creates a new index
+
+  // Methods and Properties 
+  cities.lenght //properties
+  cities.push('sailkot') //Methods 
+ let val = cities.pop() //removes and return the value 
+ console.log(cities)  
+ console.log(val) //returned value
